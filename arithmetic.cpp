20e9fde2ff51dcce53ccce813ac76450424aa3fb
delapenakm <delapena_katie@wheatoncollege.edu>
2025-04-16 16:03:53 -0400

    katie step

---
+++ arithmetic.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Kathleen de la Pena on 4/16/25.
+//
+#include <iostream>
+#include "arithmetic.h"
+
+using namespace std;
+
+int sum(int a, int b) {
+  return a + b;
+}
+int sub(int a, int b) {
+  return a - b;
+}
+int multiply(int a, int b) {
+  return a * b;
+}
+int divide(int a, int b) {
+  return a / b;
+}
