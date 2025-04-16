600871800fed884c1f3b0ae4309778ee597aaac0
Ben <kayamba_ben@wheatoncollege.edu>
2025-04-16 16:16:08 -0400

    gemometric functions

---
+++ geometry.h
@@ -0,0 +1,14 @@
+//
+// Created by Ben on 4/16/25.
+//
+
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+float areaOfCircle(float radius);
+
+float areaOfRectangle(float width, float height);
+
+float volumeOfBox(float I, float width, float height);
+
+#endif //GEOMETRY_H
