8e65d2665ae6d0f8d2dbbfb3c32ba5a05c3a9b59
sruginski <ruginski_savannah@wheatoncollege.edu>
2025-04-16 16:35:30 -0400

    changed geo back

--- geometry.cpp
+++ geometry.cpp
@@ -1,1 +1,19 @@
-don't merge this
+#include "geometry.h"
+using namespace std;
+#include <iostream>
+
+float areaOfCircle(float radius) {
+    //cout<<"Area of Circle: "<<radius<<endl;
+    return 3.14*radius*radius;
+
+
+}
+float areaOfRectangle(float width, float height) {
+ //   cout<<"Area of Rectangle: "<<width<<" "<<height<<endl;
+    return width*height;
+}
+
+float volumeOfBox(float I, float width, float height) {
+   // cout<<"Volume of Box: "<<width<<" "<<height<<endl;
+    return I*width*height;
+}
